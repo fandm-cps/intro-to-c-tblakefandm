bbcb7fe51890d75255d5b045c3f8cb5951f885e9
tblake <tblake@cs-122>
2019-09-02 14:47:42 -0400

    Made a guess game

---
+++ guess.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//what we need to use: cin/cout = done, while loop = done, if-statements = done, a hard coded reference number = done
+int main(){
+    int givennum = 19;
+    int guessnum = 0;
+
+    while(givennum != guessnum){
+        cout << "Please guess an integer." << endl;
+        cin >> guessnum;
+
+        if(guessnum > givennum){
+            cout << "Incorrect Guess. You are too high." << endl;
+        }
+        if(guessnum < givennum){
+            cout << "Incorrect Guess. You are too low." << endl;
+        }
+        if(guessnum == givennum){
+            cout << "Correct!" << endl;
+            break;
+        }
+        
+    }
+    
+
+}
